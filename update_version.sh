fd656ef2a0496b956f34bad688c2e2d647e64efa
Sybren A. Stüvel <sybren@blender.org>
2026-04-06 15:52:47 +0200

    update_version.sh: update for BAT v2
    BAT v2 uses Zensical for documentation (which doesn't include the version
    number right now), and replaced Poetry with UV.

--- update_version.sh
+++ update_version.sh
@@ -5,19 +5,18 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-poetry version $1
-sed 's/version = "[^"]*"/version = "$1"/' -i  docs/conf.py
-sed 's/release = "[^"]*"/release = "$1"/' -i docs/conf.py
+uv version $1
 sed "s/__version__\s*=\s*\"[^']*\"/__version__ = \"$1\"/" -i src/blender_asset_tracer/__init__.py
 sed --posix "s/\(dist\/blender[_-]asset[_-]tracer-\)\([0-9.betalphdv-]*[0-9]\)/\1$1/g" -i README.md
 
 git diff
 echo
 echo "Don't forget to commit and tag:"
-echo git commit -m \'Bumped version to $1\' pyproject.toml src/blender_asset_tracer/__init__.py docs/conf.py README.md
+echo git commit -m \'Bumped version to $1\' pyproject.toml src/blender_asset_tracer/__init__.py README.md
 echo git tag -a v$1 -m \'Tagged version $1\'
 echo
 echo "Build the package & upload to PyPi using:"
-echo "poetry build"
-echo "poetry run twine check dist/blender_asset_tracer-$1.tar.gz dist/blender_asset_tracer-$1-*.whl"
-echo "poetry run twine upload -r bat dist/blender_asset_tracer-$1.tar.gz dist/blender_asset_tracer-$1-*.whl"
+echo "uv build"
+echo "uv sync --group release"
+echo "uv run twine check dist/blender_asset_tracer-$1.tar.gz dist/blender_asset_tracer-$1-*.whl"
+echo "uv run twine upload -r bat dist/blender_asset_tracer-$1.tar.gz dist/blender_asset_tracer-$1-*.whl"
